af94d82ec410183eed5056d60f821fdd5d28dfbc
Nick Martinson <nick-martinson@uiowa.edu>
2015-04-15 16:45:11 -0500

    Added PanicAR

---
+++ Pods/PanicAR/Framework/PanicARKit.framework/Versions/A/Headers/PanicARKit.h
@@ -0,0 +1,27 @@
+//
+//  PanicARKit.h
+//  PanicARKit
+//
+//  Created by Andreas Zeitler on 23.11.10.
+//  copyright 2013 doPanic. All rights reserved.
+//
+
+#import "PARController+Config.h"
+#import "PARController+Objects.h"
+#import "PARController.h"
+#import "PARControllerDelegate.h"
+#import "PARObjectDelegate.h"
+#import "PARPoi.h"
+#import "PARPoiAdvancedLabel.h"
+#import "PARPoiLabel.h"
+#import "PARPoiLabelTemplate.h"
+#import "PARRadarView.h"
+#import "PARView.h"
+#import "PARViewController.h"
+#import "PARPoiFactory.h" 
+#import "PanicSensorKit.h"
+#import "PSKDeviceAttitude.h"
+#import "PSKDeviceProperties.h"
+#import "PSKMath.h"
+#import "PSKSensorDelegate.h"
+#import "PSKSensorManager.h"
